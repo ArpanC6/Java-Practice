9a516fe49e6e2121bbb1ea041a8e8aa6686355e8
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2025-12-24 09:58:25 +0530

    Create VariablesDemo.java
    Day 02: Java variables and data types practice

---
+++ Day-02/VariablesDemo.java
@@ -0,0 +1,16 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+
+        int age = 21;
+        double marks = 85.5;
+        char grade = 'A';
+        boolean isPassed = true;
+        String name = "Arpan";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + isPassed);
+    }
+}
