faf6a2fffe554345e0a37f79cdb7e6827965c873
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2025-12-30 09:13:07 +0530

    Add PrintMessage class with a welcome message
    Day 08: Method with no return type example

---
+++ Day-08/PrintMessage.java
@@ -0,0 +1,10 @@
+public class PrintMessage {
+
+    static void showMessage() {
+        System.out.println("Welcome to Java Methods!");
+    }
+
+    public static void main(String[] args) {
+        showMessage();
+    }
+}
