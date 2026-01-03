d69976f899216b66d5250f5d7b6e2f17b505f1e5
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2026-01-03 10:03:16 +0530

    Add ArrayBasics class to demonstrate array usage
    Day 12: Learned Java array basics and traversal

---
+++ Day-12/ArrayBasics.java
@@ -0,0 +1,10 @@
+public class ArrayBasics {
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("Array length: " + numbers.length);
+        System.out.println("First element: " + numbers[0]);
+        System.out.println("Last element: " + numbers[numbers.length - 1]);
+    }
+}
