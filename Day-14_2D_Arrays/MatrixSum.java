6ed955530de49b551a8bc4f46b41fdc54ca234fe
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2026-01-05 09:56:01 +0530

    Add MatrixSum class to calculate matrix sum
    Add Day 14 2D array problems

---
+++ Day-14_2D_Arrays/MatrixSum.java
@@ -0,0 +1,18 @@
+public class MatrixSum {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        int sum = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                sum += matrix[i][j];
+            }
+        }
+
+        System.out.println("Sum of matrix elements: " + sum);
+    }
+}
