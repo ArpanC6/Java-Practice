b8c0164c099516d9f3cf03646be36b0210755ca0
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2026-01-06 09:43:24 +0530

    Add CountWords class to count words in a string
    Add Day 15 string problems

---
+++ Day-15_Strings/CountWords.java
@@ -0,0 +1,14 @@
+public class CountWords {
+    public static void main(String[] args) {
+        String str = "Java is easy to learn";
+        int count = 1;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ' && str.charAt(i + 1) != ' ') {
+                count++;
+            }
+        }
+
+        System.out.println("Number of words: " + count);
+    }
+}
