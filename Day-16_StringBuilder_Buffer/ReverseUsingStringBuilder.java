122fbdd9681c7828c630a85aa25a9dc57cdbce5f
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2026-01-07 10:42:18 +0530

    Create ReverseUsingStringBuilder.java
    Add Day 16 StringBuilder and StringBuffer practice

---
+++ Day-16_StringBuilder_Buffer/ReverseUsingStringBuilder.java
@@ -0,0 +1,11 @@
+public class ReverseUsingStringBuilder {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+
+        StringBuilder sb = new StringBuilder(str);
+        sb.reverse();
+
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + sb);
+    }
+}
