373249c8175ef8e190961aba2f0b7bb0a287433a
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2026-01-07 10:43:18 +0530

    Add StringBufferExample to demonstrate StringBuffer
    Add Day 16 StringBuilder and StringBuffer practice

---
+++ Day-16_StringBuilder_Buffer/StringBufferExample.java
@@ -0,0 +1,10 @@
+public class StringBufferExample {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello");
+
+        sb.append(" World");
+        sb.reverse();
+
+        System.out.println(sb);
+    }
+}
