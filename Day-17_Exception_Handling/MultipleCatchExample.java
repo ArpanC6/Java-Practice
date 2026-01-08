ba66747d723bd779ca686a2b8132bae9f0caf97e
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2026-01-08 10:43:25 +0530

    Add MultipleCatchExample for exception handling
    Add Day 17 exception handling practice

---
+++ Day-17_Exception_Handling/MultipleCatchExample.java
@@ -0,0 +1,14 @@
+public class MultipleCatchExample {
+    public static void main(String[] args) {
+        try {
+            int[] arr = {1, 2, 3};
+            System.out.println(arr[5]);
+        } catch (ArithmeticException e) {
+            System.out.println("Arithmetic Exception");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array Index Out Of Bounds Exception");
+        } catch (Exception e) {
+            System.out.println("General Exception");
+        }
+    }
+}
