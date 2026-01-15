490093adc66d6a504688f72e8d55c80b05d84435
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2026-01-15 09:57:05 +0530

    Add IteratorExample class to demonstrate iterators
    Add Day 25: Iterator and ListIterator examples with fail-fast behavior

---
+++ Day25_Iterators/IteratorExample.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class IteratorExample {
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("Python");
+        list.add("C++");
+
+        Iterator<String> it = list.iterator();
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
