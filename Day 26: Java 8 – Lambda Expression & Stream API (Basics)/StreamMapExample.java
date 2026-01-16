0139032473add930a0fbf1cd7dcb126036e6fafb
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2026-01-16 10:43:21 +0530

    Add StreamMapExample to demonstrate stream mapping

---
+++ Day 26: Java 8 – Lambda Expression & Stream API (Basics)/StreamMapExample.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class StreamMapExample {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        numbers.stream()
+               .map(n -> n * n)
+               .forEach(System.out::println);
+    }
+}
