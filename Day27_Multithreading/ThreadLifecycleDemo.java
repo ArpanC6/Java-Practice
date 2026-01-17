ebd79689463a0eec7e33b826c1253e1bcebbf056
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2026-01-17 09:40:19 +0530

    Add ThreadLifecycleDemo to demonstrate thread states
    Day 27: Added multithreading basics using Thread and Runnable

---
+++ Day27_Multithreading/ThreadLifecycleDemo.java
@@ -0,0 +1,15 @@
+class LifeCycle extends Thread {
+    public void run() {
+        System.out.println("Thread is running");
+    }
+}
+
+public class ThreadLifecycleDemo {
+    public static void main(String[] args) {
+        LifeCycle t = new LifeCycle();
+
+        System.out.println("Thread State after creation: " + t.getState());
+        t.start();
+        System.out.println("Thread State after start: " + t.getState());
+    }
+}
