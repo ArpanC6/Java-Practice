e4553cd04d74c46f90ce200425c616ee511248c6
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2026-01-24 10:00:01 +0530

    Add Student class with properties and constructors

---
+++ Day34_SpringBoot_CRUD_REST_API/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    private int id;
+    private String name;
+
+    public Student() {}
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // getters & setters
+}
