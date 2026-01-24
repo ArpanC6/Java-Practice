7c3140a2be9cd8be3a800f7df8afeec274909c62
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2026-01-24 10:03:30 +0530

    Add GlobalExceptionHandler for centralized error handling

---
+++ Day34_SpringBoot_CRUD_REST_API/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
