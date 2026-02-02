a734f9cef932d92102567743cc503d1125b5ad86
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2026-02-02 10:14:38 +0530

    Create JwtUtil.java

---
+++ day43-role-based-auth/JwtUtil.java
@@ -0,0 +1,30 @@
+@Component
+public class JwtUtil {
+
+    private final String SECRET_KEY = "rolejwt123";
+
+    public String generateToken(String username, String role) {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public Claims extractClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public String extractRole(String token) {
+        return extractClaims(token).get("role", String.class);
+    }
+}
