1eb224afa4afdb3f3a33141d5d3b004de69a5499
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2026-02-03 10:35:32 +0530

    Add JwtUtil class for JWT token generation and claims extraction

---
+++ day44-refresh-token/JwtUtil.java
@@ -0,0 +1,31 @@
+@Component
+public class JwtUtil {
+
+    private final String SECRET_KEY = "refreshjwtkey";
+
+    public String generateAccessToken(String username, String role) {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 15))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String generateRefreshToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24 * 7))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public Claims extractClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
